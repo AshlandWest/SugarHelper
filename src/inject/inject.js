73187c9f1fba6c3bbea08d8b629fea3e736e15b2
Ash West <ashlandmwest@gmail.com>
2020-11-05 01:00:57 -0800

    Add ALL THE THINGS

---
+++ src/inject/inject.js
@@ -0,0 +1,41 @@
+//Add needed font for clipboard button to the page
+function addFonts() {
+  let node = document.createElement("link");
+  node.setAttribute("rel", "stylesheet");
+  node.setAttribute(
+    "href",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  );
+  document.head.appendChild(node);
+}
+addFonts();
+var emailElements = [];
+var readyStateCheckInterval = setInterval(function () {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
+
+    // ----------------------------------------------------------
+    // This part of the script triggers when page is done loading
+    console.log("Plugin is running");
+
+    emailElements.push(
+      ...document.querySelectorAll("a[href='javascript:void(0);']")
+    );
+    function makeButton(emailAddress) {
+      let button = document.createElement("button");
+      let copyIcon = document.createElement("i");
+      copyIcon.classList.add("fa", "fa-copy");
+      button.appendChild(copyIcon);
+      button.value = emailAddress;
+      button.addEventListener("click", (event) => {
+        navigator.clipboard.writeText(button.value);
+      });
+      return button;
+    }
+    console.log(emailElements);
+    emailElements.forEach((element) => {
+      element.after("\xa0", makeButton(`${element.innerText}`));
+    });
+    // ----------------------------------------------------------
+  }
+});
